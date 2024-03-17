17a7d3fccb17e8ab7a0c2925688e6e5b79cf54da
KindaZhang <2930767060@qq.com>
2024-03-18 01:05:52 +0800

    24-03-18

---
+++ follow_jackfrued_100/code/day01/example/painting.py
@@ -0,0 +1,13 @@
+import turtle #引用turtle
+turtle.pensize(4)#pensize()函数用来指定绘制线条的粗细
+turtle.pencolor('red')#线条的颜色
+
+turtle.forward(100)#线条向前100个单位
+turtle.right(90)#右拐90度
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()#一个无限循环，用来坚挺和处理用户的事件。这里就是保持饥饿买呢一直存在，直到用户关闭
---
+++ follow_jackfrued_100/code/day01/example/quote.py
@@ -0,0 +1,13 @@
+"""
+import tkinter
+
+print(tkinter._test())
+"""
+
+"""
+import tkinter
+
+print(tkinter.TkVersion)
+"""
+
+import this
